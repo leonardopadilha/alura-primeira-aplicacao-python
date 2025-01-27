715c24f84ed4bfc343e99230174615f8c640e1e7
leonardopadilha <leonardopadilha28@gmail.com>
2025-01-27 06:15:09 -0300

    Adicionando property e classmethod

--- aplicando_orientacao_objetos/sabor_express/modelos/restaurante.py
+++ aplicando_orientacao_objetos/sabor_express/modelos/restaurante.py
@@ -3,25 +3,36 @@ class Restaurante:
   restaurantes = []
   
   def __init__(self, nome, categoria):
-    self.nome = nome
-    self.categoria = categoria
-    self.ativo = False
+    self._nome = nome.title()
+    self._categoria = categoria.upper()
+    self._ativo = False
     Restaurante.restaurantes.append(self)
 
   def __str__(self):
-    return f'{self.nome} | {self.categoria}'
+    return f'{self._nome} | {self._categoria}'
 
-  def listar_restaurantes():
-    for restaurante in Restaurante.restaurantes:
-      print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+  @classmethod 
+  def listar_restaurantes(cls):
+    dados_restaurante = ['Nome do restaurante', 'Categoria', 'Status']
+    print(f'{dados_restaurante[0].ljust(25)} | {dados_restaurante[1].ljust(25)} | {dados_restaurante[2]}')
+    for restaurante in cls.restaurantes:
+      print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+  @property # Modificando como o atributo será lido
+  def ativo(self):
+    return '☑' if self._ativo else '☐'
+  
+  def alternar_estado(self):
+    self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 print(vars(restaurante_praca)) # Exibe as informações da classe como um dicionário
 # print(dir(restaurante_praca)) # Exibe os métodos disponíveis para aquela classe
 
-print(restaurante_praca)
+#print(restaurante_praca)
 
 print()
 
