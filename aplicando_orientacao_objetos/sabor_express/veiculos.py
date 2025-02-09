47890118d354dcd1c75c75538ff38dc952c24b2d
leonardopadilha <leonardopadilha28@gmail.com>
2025-02-09 06:50:22 -0300

    Criando veículos com orientação a objetos

---
+++ aplicando_orientacao_objetos/sabor_express/veiculos.py
@@ -0,0 +1,8 @@
+from modelos.carro import Carro
+from modelos.moto import Moto
+
+carro1 = Carro("Toyota", "Corolla", 4)
+moto1 = Moto("Harley-Davidson", "Street 750", "Esportiva")
+
+print(carro1)
+print(moto1)
